e739516595d7e33246579230fb1fe13d597601d4
Sharma Ji <66856959+sakhiofsakshi@users.noreply.github.com>
2021-10-20 16:45:38 +0530

    Build vendor interfaces libraries

--- kona.mk
+++ kona.mk
@@ -88,6 +88,7 @@ $(call inherit-product, $(LOCAL_PATH)/kona/audio/audio.mk)
 
 #Bluetooth
 PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.1.vendor \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor
      
@@ -127,6 +128,9 @@ PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := verify
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.3-service.clearkey
     
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.4.vendor
+    
 # Exclude sensor from InputManager
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/excluded-input-devices.xml:system/etc/excluded-input-devices.xml \
@@ -138,6 +142,15 @@ PRODUCT_PACKAGES += \
 # Freeform Multiwindow
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
+    
+# Gatekeeper
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0.vendor
+
+# GPS
+PRODUCT_PACKAGES += \
+    android.hardware.gnss@1.1.vendor \
+    android.hardware.gnss@2.1.vendor
 
 # Health
 PRODUCT_PACKAGES += \
@@ -170,6 +183,10 @@ PRODUCT_PACKAGES += \
     IPACM_cfg.xml \
     libipanat \
     liboffloadhal
+    
+# Keymaster
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@4.1.vendor
     
 # Lights
 PRODUCT_PACKAGES += \
@@ -213,6 +230,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
     
+PRODUCT_PACKAGES += \
+    android.system.net.netd@1.1.vendor \
+
+# Neural networks
+PRODUCT_PACKAGES += \
+    android.hardware.neuralnetworks@1.3.vendor
+    
 # NFC
 PRODUCT_PACKAGES += \
     com.android.nfc_extras \
@@ -227,7 +251,7 @@ PRODUCT_PACKAGES += \
     vendor.nxp.hardware.nfc@2.0.vendor \
     android.hardware.secure_element@1.0 \
     android.hardware.secure_element@1.1 \
-    android.hardware.secure_element@1.2
+    android.hardware.secure_element@1.2.vendor \
     
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
@@ -330,6 +354,11 @@ PRODUCT_PACKAGES += \
     libprotobuf-cpp-full \
     librmnetctl \
     libxml2
+    
+PRODUCT_PACKAGES += \
+android.hardware.radio@1.6.vendor \
+android.hardware.radio.config@1.3.vendor \
+android.hardware.radio.deprecated@1.0.vendor
     
 #Recovery
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/recovery/ramdisk,$(TARGET_COPY_OUT_RECOVERY)/root)
